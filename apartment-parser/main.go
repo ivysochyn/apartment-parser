9554d3cce7017fb7251667c5f3e4b1f845569b74
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-04-10 19:19:25 +0200

    [#3] apartment-parser: main.go: Add function to fetch HTML page and write it to a file

--- apartment-parser/main.go
+++ apartment-parser/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
+// fetchHTMLPage fetches the HTML page at the given URL and stores it in the given path.
+// If the path is empty, the page is stored in the current directory.
+func fetchHTMLPage(url string, path string) {
+	if path == "" {
+		path = "./index.html"
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create the file if it doesn't exist
+	// overwrites the file if it already exists
+	file, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	// Write the body to file
+	_, err = file.Write(body)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	fetchHTMLPage("https://www.google.com", "")
 }
