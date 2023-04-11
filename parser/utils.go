6a5dd68d7f7073d073565a0027b47a3399755a04
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-04-11 22:52:59 +0200

    [#6] parser: utils.go: Refactor FetchHTMLPage function
    Removes function read the HTML from file.
    
    Refactors the 'FetchHTMLPage' function to return string of body instead of saving it to a specified file

--- parser/utils.go
+++ parser/utils.go
@@ -2,51 +2,30 @@ package parser
 
 import(
 	"net/http"
-	"os"
 	"io/ioutil"
 )
 
-// fetchHTMLPage fetches the HTML page at the given URL and stores it in the given path.
-// If the path is empty, the page is stored in the current directory.
-func FetchHTMLPage(url string, path string) {
-	if path == "" {
-		path = "./index.html"
-	}
-
+// FetchHTMLPage fetches the HTML page from the given URL
+// and returns the HTML page as a string.
+// If an error occurs, it returns an empty string and the error.
+//
+// Example:
+//  html, err := FetchHTMLPage("https://www.google.com")
+//  if err != nil {
+//      // handle error
+//  }
+func FetchHTMLPage(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+        return "", err
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+        return "", err
 	}
 
-	// Create the file if it doesn't exist
-	// overwrites the file if it already exists
-	file, err := os.Create(path)
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-
-	// Write the body to file
-	_, err = file.Write(body)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func ReadHtmlFromFile(filename string) (string, error) {
-	bs, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		return "", err
-	}
-
-	return string(bs), nil
+    return string(body), nil
 }
