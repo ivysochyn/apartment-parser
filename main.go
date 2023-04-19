a5310103603bdcdd8f2babf00d9dd8c08bbf9dce
Illia Vysochyn <ivysochyn@internships.antmicro.com>
2023-04-19 13:19:34 +0200

    [#19] main.go: Start the bot

--- main.go
+++ main.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"apartment-parser/database"
-	"apartment-parser/parser"
-	"fmt"
-	"log"
+	"apartment-parser/telegrambot"
 )
 
 func main() {
-	url := "https://www.olx.pl/poznan/q-mieszkanie/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=1000&search%5Bfilter_float_price:to%5D=3000"
-	db, err := database.CreateDatabase("apartments.db")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer db.Close()
-
-	body, err := parser.FetchHTMLPage(url)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data := parser.ParseHtml(body)
-	for _, offer := range data {
-		err := database.AddOffer(db, offer)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	all_offers, _ := database.ListOffers(db)
-	for _, offer := range all_offers {
-		fmt.Println(offer)
-	}
+	telegrambot.StartBot(false)
 }
